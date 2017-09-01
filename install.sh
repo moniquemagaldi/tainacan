122241ede43d65bfa99eaab2c074712f020db1a7
walisonjose <walison.josededeus@gmail.com>
2017-09-01 15:27:58 -0300

    Testando a integração contínua

--- install.sh
+++ install.sh
@@ -6,8 +6,23 @@ eval `ssh-agent -s` #start shh agent
 ssh-add ~/.ssh/deploy-key
 chmod 600 config.txt
 mv config.txt ~/.ssh/config
-pwd
-ls
+
+
+if [ $TRAVIS_BRANCH == 'continuousIntegration' ] ; then
+    # Initialize a new git repo in current dir, and push it to our server.
+    git init
+
+    git remote add deploy "$SERVER_USER@$SERVER_ADDRESS:$GIT_PATH"
+    git config user.name "Travis CI"
+    git config user.email "$DEPLOY_EMAIL"
+
+    git add .
+    git commit -m "Deploy"
+    git push --force deploy continuousIntegration -v
+else
+    echo "Not deploying, since this branch isn't master."
+fi
+
 
 
 #cat deploy-key >> ~/.ssh/authorized_keys
